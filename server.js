3f29bd41f1533dd2d898a1e248cee2caeea31607
r_Richa <69792227+raoricha@users.noreply.github.com>
2023-04-16 01:41:25 +0530

    updated

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ import categoryRoutes from './routes/category.js'
 import productRoutes from './routes/product.js'
 import cors from 'cors'
 import path from 'path'
+import {fileURLToPath} from 'url'
 
 dotenv.config()
 
 connectDB()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
